7f4c57cc1e81f3175791ddaeadb11836f8006822
ellegenzi <106830513+ellegenzi@users.noreply.github.com>
2022-10-15 11:55:15 -0400

    Adding final challenge files.

--- static/js/charts.js
+++ static/js/charts.js
@@ -3,7 +3,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
+  d3.json("static/data/samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -32,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
+  d3.json("static/data/samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -56,23 +56,40 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("static/data/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samplesArray = data.samples;
+
+    // Deliverable 3 Secret Step
+    // Create a variable that holds the metadata array.
+    var metaArray = data.metadata;
+
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     var filterSamples = samplesArray.filter(sampleObj => sampleObj.id == sample);
+    
+    // Deliverable 3
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var filteredMeta = metaArray.filter(sampleObj => sampleObj.id == sample);
+
     //  5. Create a variable that holds the first sample in the array.
     var firstSample = filterSamples[0];
+
+    // Deliverable 3
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var firstMeta = filteredMeta[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otuIds = firstSample.otu_ids;
     var otuLabels = firstSample.otu_labels;
     var sampleValues = firstSample.sample_values;
 
+    // Deliverable 3
+    // 3. Create a variable that holds the washing frequency.
+    var washingFreq = parseFloat(firstMeta.wfreq);
+
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
-
     var yticks = otuIds.slice(0, 10).map(otuIds => `OTU ${otuIds}`).reverse();
 
     // 8. Create the trace for the bar chart. 
@@ -129,6 +146,44 @@ function buildCharts(sample) {
     };
 
     // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+    
+    // Deliverable 3
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [
+      {
+        domain: {x: [0,1], y: [0,1]},
+        value: washingFreq,
+        title: {text: '<b>Belly Button Washing Frequency<b><br>Scrubs per Week<br>'},
+        type: 'indicator',
+        mode: 'gauge+number',
+        gauge: {
+          axis: {range: [null, 10]},
+          bar: {color: 'black'},
+          steps: [
+            {range: [0,2], color: 'red'},
+            {range: [2,4], color: 'orange'},
+            {range: [4,6], color: 'yellow'},
+            {range: [6,8], color: 'lightgreen'},
+            {range: [8,10], color: 'green'}
+          ]
+        }
+      }
+     
+    ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = {
+      width: 500,
+      height: 400,
+      margin: {
+        t: 0,
+        b: 0
+      } 
+     
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 }
